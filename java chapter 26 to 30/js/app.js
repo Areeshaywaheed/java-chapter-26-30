5b4668fb9f74f54b6f141760a9503f64e0ecd8b1
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-06-25 21:22:17 +0500

    Add files via upload

---
+++ java chapter 26 to 30/js/app.js
@@ -0,0 +1,107 @@
+             // ===========chapter 26 to 30 started==========
+
+// =============Q-1================
+
+// var number=3.45214
+// var rate=Math.round(number)
+// var floor=Math.floor(number)
+// var ceil=Math.ceil(number)
+// document.write("round off value: ",rate,"<br>","floor value: ",floor,"<br>","ceil value: ",ceil)
+
+
+// =============Q-2================
+
+// var number=-2.673
+// var rate=Math.round(number)
+//  var floor=Math.floor(number)
+//  var ceil=Math.ceil(number)
+//  document.write("round off value: ",rate,"<br>","floor value: ",floor,"<br>","ceil value: ",ceil)
+
+
+// =============Q-3================
+
+// var num = +prompt("enter number");
+// var absolute = Math.abs(num);
+// document.write("The absolute value of " + num + " is " + absolute);
+
+
+// =============Q-4================
+
+//  var randomNo =Math.random();
+//  var maths = (randomNo * 6) + 1;
+//  var diceNumber = Math.floor(maths);
+//  document.write("random dice value " + diceNumber);
+
+// =============Q-5================
+
+//  var valueOfCoin = Math.random() * 3;
+//  var floor = Math.floor(valueOfCoin);
+//  if (floor < 2){
+//      document.write(floor + "<br>" + " random coin value: " + "Tails" )
+//  }
+//  else{
+//      document.write(floor + "<br>" + " random coin value: " + "Heads")
+//  }
+
+// =============Q-6================
+
+//  var randomNum = Math.random() * 100 + 1 ;
+//  var floor = Math.floor(randomNum);
+//  document.write("random number between 1 and 100: " + floor );
+
+// =============Q-7================
+
+//  var userWeight = prompt("enter your weight in kilograms");
+//  var parse = parseFloat(userWeight);
+//  document.write("The weight of user is " + parse.toFixed(1) + " kilograms");
+
+// =============Q-8================
+
+// var randomNo =Math.ceil( (Math.random() * 10)) ;
+//  document.write(randomNo);
+//  var askUser = +prompt("Input a number between 1 to 10");
+//   if(askUser === randomNo){
+//      alert("Congratulations!");}
+
+//  else{
+//      alert("Try again!");
+// }
+
+
+
+               // ===========chapter 26 to 30 completed==========
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
